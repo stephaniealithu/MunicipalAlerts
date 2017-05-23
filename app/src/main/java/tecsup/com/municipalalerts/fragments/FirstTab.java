6320daf29f2662d4bcf05cb7d0325b07b8260c9b
Stephanie Granados <stephanie.granados@tecsup.edu.pe>
2017-05-23 09:15:26 -0500

    Changesss

--- app/src/main/java/tecsup/com/municipalalerts/fragments/FirstTab.java
+++ app/src/main/java/tecsup/com/municipalalerts/fragments/FirstTab.java
@@ -33,22 +33,7 @@ public class FirstTab extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        contactsList = (ListView) view.findViewById(R.id.contacts_list);
 
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://municipalalerts2017.firebaseio.com/Users");
-        FirebaseListAdapter<String> firebaseListAdapter = new FirebaseListAdapter<String>(
-                getActivity(),
-                String.class,
-                android.R.layout.simple_list_item_1,
-                databaseReference){
-
-            @Override
-            protected void  populateView(View v, String model, int position){
-                TextView textView = (TextView) v.findViewById(android.R.id.text1);
-                textView.setText(model);
-            }
-        };
-        contactsList.setAdapter(firebaseListAdapter);
 
 
     }
